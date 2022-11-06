fc3c5dc6a20dcbb48768f9875bc5f95c4da85160
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-11-06 20:51:14 +0300

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_113221) do
+ActiveRecord::Schema.define(version: 2022_11_06_174102) do
 
   create_table "games", force: :cascade do |t|
     t.string "title"
